613138a2a5d73f95f8d7faef1eef026e2fce4fd0
Tyler Kummer <tylerkummer@protonmail.com>
2020-04-29 15:57:48 -0500

    Commit

--- api/server.js
+++ api/server.js
@@ -8,6 +8,9 @@ const essentialsRouter = require("../essentials/essentials-router.js");
 const projectsRouter = require("../projects/projects-router.js");
 const missionRouter = require("../mission/mission-router.js");
 
+// Router for our Authenticator (Sprint Challenge Example)
+// const jokesRouter = require("../jokes/jokes-router.js");
+
 const server = express();
 
 server.use(helmet());
@@ -18,5 +21,8 @@ server.use("/api/auth", authRouter);
 server.use("/api/essentials", authenticate, essentialsRouter);
 server.use("/api/projects", authenticate, projectsRouter);
 server.use("/api/mission", authenticate, missionRouter);
+
+// Server for our authenticator (Sprint challenge example)
+// server.use("/api/jokes", authenticate, jokesRouter);
 
 module.exports = server;
